696da5c829c605cb7a500694a6d47e36e0eea9fb
Tina Bellon <bellon.tina@gmail.com>
2021-11-28 23:24:12 -0600

    updated schema

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-     emp_no VARCHAR NOT NULL,
+     emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
 	 first_name VARCHAR NOT NULL,
 	 last_name VARCHAR NOT NULL,
@@ -15,10 +15,6 @@ CREATE TABLE employees (
 	 hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no)
 );
-
-ALTER TABLE employees
-ALTER COLUMN emp_no TYPE INT
-USING emp_no::integer;
 
 CREATE TABLE dept_manager (
 	 dept_no VARCHAR(4) NOT NULL,
@@ -39,23 +35,22 @@ CREATE TABLE salaries (
 	 PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE titles (
-	 emp_no INT NOT NULL,
-	 title VARCHAR NOT NULL,
-	 from_data DATE NOT NULL,
-	 to_date DATE NOT NULL,
-	 FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	 PRIMARY KEY (emp_no)
-);
 
 CREATE TABLE dept_emp (
 	 emp_no INT NOT NULL,
 	 dept_no VARCHAR(4) NOT NULL,
 	 from_date DATE NOT NULL,
 	 to_date DATE NOT NULL,
-	 FOREIGN KEY (emp_no) REFERENCES titles (emp_no),
+	 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	 PRIMARY KEY (emp_no, dept_no)
 );
 
-SELECT * FROM dept_emp;
+
+CREATE TABLE titles (
+	 emp_no INT NOT NULL,
+	 title VARCHAR NOT NULL,
+	 from_data DATE NOT NULL,
+	 to_date DATE NOT NULL,
+	 FOREIGN KEY (emp_no) REFERENCES employees (emp_no) 
+);
